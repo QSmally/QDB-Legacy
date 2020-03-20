6b289dbea77315c742bfb483274fb0181661806f
QSmally <github@qbot.eu>
2020-03-20 11:30:44 +0100

    Tested pools and DataStores

--- Test/Main.js
+++ Test/Main.js
@@ -2,3 +2,14 @@
 process.on("QDB-Debug", Debug => console.log(Debug));
 const QDB = require("../QDB");
 // const DB  = new QDB.Connection("./Test/Database.json");
+
+// const DS = new QDB.DataStore();
+// DS.set("foo", {bar: "roo"});
+// DS.set("bar", {foo: "roo"});
+// DS.set("roo", {foo: "bar"});
+
+// console.log( DS.resolve("bar") );
+
+
+// const DBS = new QDB.Pool("./Test/");
+// console.log( DBS.Select("Database") );
