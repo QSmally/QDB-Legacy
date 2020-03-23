d1f53a486aeb89fccd52c0f66a6dc847fc55743c
QSmally <github@qbot.eu>
2020-03-23 15:19:53 +0100

    Tested new debug system

--- Test/Main.js
+++ Test/Main.js
@@ -1,21 +1,5 @@
 
-process.on("QDB-Debug", Debug => console.log(Debug));
 const QDB = require("../QDB");
-// const DB  = new QDB.Connection("./Test/Database.json");
+const DBS = new QDB.Pool("./Test/");
 
-class TestingClass {
-    constructor () {
-        this.foo = "bar";
-    }
-}
-
-const DS = new QDB.DataStore();
-DS.set("foo", new TestingClass());
-DS.set("bar", new TestingClass());
-DS.set("roo", new TestingClass());
-
-console.log( DS.resolve("bar") );
-
-
-// const DBS = new QDB.Pool("./Test/");
-// console.log( DBS.Select("Database") );
+DBS.OnEvent(m => console.log(m));
