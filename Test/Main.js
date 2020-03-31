a2b8f693a443c44418e0c9352300cbccf9c19c8c
QSmally <github@qbot.eu>
2020-03-31 11:25:56 +0200

    Testing Fetch

--- Test/Main.js
+++ Test/Main.js
@@ -1,9 +1,9 @@
 
 const QDB = require("../QDB");
+const DB = new QDB.Connection("./Test/Database.json");
+
 
 // Test: Connection#Invert
-
-const DB = new QDB.Connection("./Test/Database.json");
 
 DB.Set({
     foo: {
@@ -14,13 +14,24 @@ DB.Set({
     a: {b: {c: "Hello World!"}}
 });
 
-console.log( DB.Get() );
+// console.log( DB.Get() );
 
 
-DB.Invert("foo.bar"); // true
-DB.Invert("foo.roo"); // false
-DB.Invert("roo");     // false
-DB.Invert("a.b.c");   // null
-DB.Invert("bar");     // undefined
+// DB.Invert("foo.bar"); // true
+// DB.Invert("foo.roo"); // false
+// DB.Invert("roo");     // false
+// DB.Invert("a.b.c");   // null
+// DB.Invert("bar");     // undefined
 
-console.log( DB.Get() );
+// console.log( DB.Get() );
+
+
+// Test: Connection#Fetch
+
+console.log( DB.Fetch("roo") );
+console.log( DB.Fetch("foo", "bar") );
+console.log( DB.Fetch("a", "b.c.length") );
+
+console.log( DB.Get("roo") );
+console.log( DB.Get("foo", "bar") );
+console.log( DB.Get("a", "b.c.length") );
